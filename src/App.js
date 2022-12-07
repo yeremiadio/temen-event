c8c3483cff955cd4220f16329295adb77661bbe5
yeremiadio <raikkonendio@gmail.com>
2022-12-07 22:21:32 +0700

    versi 2 ke versi 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Temen Event Versi 2
+          Temen Event Versi 3
         </p>
         <a
           className="App-link"
